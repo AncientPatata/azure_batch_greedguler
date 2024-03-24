f03cfa5d9cfdebb7541b2b6bb1a4a7eb339e492f
AncientPatata <abde.arrahman.dk@gmail.com>
2024-03-24 15:02:04 +0100

    Automated Repo Update

---
+++ log.py
@@ -0,0 +1,14 @@
+# log_sender.py
+import requests
+
+def create_logging_function(machine_id):
+    try:
+        with open('ngrok_url.txt', 'r') as url_file:
+            ngrok_url = url_file.read().strip()
+        log_endpoint = f"{ngrok_url}/log"
+        def logger(msg):
+            requests.post(log_endpoint, json={"machine": machine_id,"message":msg},timeout=0.0000000001)
+        return logger
+    except Exception as e:
+        print(f"Error sending log message: {e}")
+        return print
--- mpi_test.py
+++ mpi_test.py
@@ -1,11 +1,16 @@
 # Save this as mpi_sum.py
 from mpi4py import MPI
 import numpy as np
+from log import *
+
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+logger = create_logging_function(rank)
+
+logger("Test")
 # Each process will have a local_num
 local_num = rank + 1
 
@@ -15,6 +20,8 @@ total_sum = comm.reduce(local_num, op=MPI.SUM, root=0)
 # Only the root process will print the result
 if rank == 0:
     print(f"Total sum is {total_sum}")
+    logger(f"Total sum is {total_sum}")
+
     # Save the result to a file
     with open('mpi_output.txt', 'w') as f:
         f.write(f"Total sum is {total_sum}")
